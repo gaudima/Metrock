34a7c042d9a5c9f3cc77e461e294e3c5bee97cbe
Dmitriy Bitsulya <gaudima@gmail.com>
2015-10-01 09:48:19 +0300

    added .gitignore

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/home/lolka/pebble-dev/PebbleSDK-3.4/Pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = [u'basalt']
